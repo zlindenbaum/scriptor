98dc15cde5812ba79a4e506282aa26208b95b202
zachary123212 <zlindenbaum@gmail.com>
2016-04-25 22:10:02 +0300

    added additional parameters for items

--- Item.py
+++ Item.py
@@ -1,6 +1,7 @@
 class Item(object):
-    def __init__(self):
+    def __init__(self, name = "", desc = "", takeDesc = "", isHidden = False, isTakeable = True):
         self.name = name
         self.desc = desc
+        self.takeDesc = takeDesc
         self.isHidden = isHidden
         self.isTakeable = isTakeable
